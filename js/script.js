b4bde2f0cd3d9536047785cef4462aaea8cb8476
LostPeril <LostPeril@mail.ru>
2022-11-20 18:07:13 +0300

    создаем таймер обратного отсчета на сайте

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
+  // tabs
   const tabs = document.querySelectorAll(".tabheader__item");
   const tabsContent = document.querySelectorAll(".tabcontent");
   const tabsParent = document.querySelector(".tabheader__items");
@@ -35,4 +36,57 @@ window.addEventListener("DOMContentLoaded", () => {
       });
     }
   });
+  //timer
+
+  const deadline = "2022-11-21";
+
+  function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    const days = Math.floor(t / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((t / 1000 / 60) % 24);
+    const seconds = Math.floor((t / 1000) % 60);
+
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  function setCloack(selector, endtime) {
+    const timer = document.querySelector(selector);
+    const days = document.querySelector("#days");
+    const hours = document.querySelector("#hours");
+    const minutes = document.querySelector("#minutes");
+    const seconds = document.querySelector("#seconds");
+    const timeInterval = setInterval(updateCloack, 1000);
+
+    updateCloack();
+
+    function updateCloack() {
+      const t = getTimeRemaining(endtime);
+
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setCloack(".timer", deadline);
 });
