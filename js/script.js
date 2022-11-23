029a0d6d95a8d1e419d48b095bcd563440a5a437
LostPeril <LostPeril@mail.ru>
2022-11-23 18:43:57 +0300

    модификация модального окна

--- js/script.js
+++ js/script.js
@@ -104,13 +104,16 @@ window.addEventListener("DOMContentLoaded", () => {
   const modalClose = document.querySelector("[data-close]");
   const modal = document.querySelector(".modal");
 
+  function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    //modal.classList.toggle("show");//аналог строчки 115 и 116
+    document.body.style.overflow = "hidden";
+    clearInterval(modalTimerId);
+  }
+
   modalTrigger.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      modal.classList.add("show");
-      modal.classList.remove("hide");
-      //modal.classList.toggle("show");//аналог строчки 115 и 116
-      document.body.style.overflow = "hidden";
-    });
+    btn.addEventListener("click", openModal);
   });
 
   function closeModal() {
@@ -133,4 +136,18 @@ window.addEventListener("DOMContentLoaded", () => {
       closeModal();
     }
   });
+
+  const modalTimerId = setTimeout(openModal, 5000);
+
+  function showModalByScroll() {
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight - 1
+    ) {
+      openModal();
+      window.removeEventListener("scroll", showModalByScroll);
+    }
+  }
+
+  window.addEventListener("scroll", showModalByScroll);
 });
